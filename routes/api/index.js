90ace36f51eeb11fa1a4aad79cd196169fedd6d8
H-meister <105766365+H-meister@users.noreply.github.com>
2022-10-19 00:16:26 -0400

    Adding functionality

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+//This file, like index.js in the models folder, will serve as a means to collect all of the API routes and package them up for us.
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+//in this file we take those routes and implement them to another router instance, prefixing them with the path /users at that time
+router.use('/users', userRoutes);
+
+module.exports = router;
