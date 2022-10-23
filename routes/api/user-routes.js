a4f0f0f349e1d3eccef1f3011d9597845bed8e0c
H-meister <105766365+H-meister@users.noreply.github.com>
2022-10-23 01:11:41 -0400

    initial set up

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     //Access our User model and run .findall() mehtod
     User.findAll({
         //we can exclude certain things from showing in this case the password. 
-        attributes: { exclude: ['password'] }
+        // attributes: { exclude: ['password'] }
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 //GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password'] },
+        // attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
@@ -58,6 +58,8 @@ router.put('/:id', (req, res) => {
 
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+        //must be added when using before update hook on hashed password.
+        individualHooks: true,
         where: {
             id: req.params.id
         }
