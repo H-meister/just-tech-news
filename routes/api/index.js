aa23f9c6a0cad280288331f7da4fe0ef8ce2f2d7
H-meister <Hector1626hg@gmail.com>
2022-10-23 23:25:16 -0400

    adding routes and finishing posts

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 //in this file we take those routes and implement them to another router instance, prefixing them with the path /users at that time
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
